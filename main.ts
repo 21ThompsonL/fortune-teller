1f14130fc583aea9eb80298ab933b6f4ca83cc73
21ThompsonL <102516341+21ThompsonL@users.noreply.github.com>
2022-03-28 10:18:59 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    if (false) {
+        basic.showString("LIFE OF SADNESS ")
+        basic.pause(500)
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showString("ACHIEVE YOUR GOALS")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
